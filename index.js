a9b1171dd5077e4d6f7fd4abf0d2e7a1bda8bfab
Tatevik999 <tatevik.arsenyan@tumo.org>
2025-07-18 14:56:11 +0400

    final version to deploy

--- index.js
+++ index.js
@@ -3,42 +3,42 @@ const{ Server } = require('socket.io');
 
 let allCounts = {};
 
-const PORT = 3000;
-const ORIGIN = 'http://localhost:5173'
+const PORT = Number(process.env.PORT);
+const ORIGIN = 'http://tumo-silvikh1-client.vercel.app';
 
 
 
-const httpserver = createServer()
-const io = new Server (httpserver, { cors: { origin: 'http://localhost:5173' }});
+const httpserver = createServer();
+const io = new Server (httpserver, { cors: { origin: ORIGIN }});
 
 
 io.on('connection',(socket) => {
-    console.log(socket.id, ' client server connection');
+ console.log(socket.id, 'client server connection');
 
-    socket.join('room');
+ socket.join('room');
 
-    if(!allCounts[socket.id]){
-        allCounts[socket.id] = 0
-    }
+ if (!allCounts[socket.id]){
+    allCounts[socket.id] = 0;
+ }
       
  
- socket.emit('update count', allCounts);
+ socket.emit('update-count', allCounts);
 
  socket.on('increase-count',() => {
     console.log(allCounts[socket.id], 'increase-count');
     allCounts[socket.id]++;
-    if (allCounts[socket.id] >= 30){
-      io.to('room').emit('winner', socket.id);
-      io.socketsLeave("room");
-    }
+     if (allCounts[socket.id] >= 30) {
+         io.to('room').emit('winner', socket.id);
+         io.socketsLeave("room");
+      }
 
     io.to('room').emit('update-count', allCounts);
- });
+   });
 
- socket.on('disconnect', () => {
-    delete allCounts [socket.id];
+   socket.on('disconnect', () => {
+    delete allCounts[socket.id];
     socket.leave('room');
- });
+   });
 });
 
 httpserver.listen(3000);
